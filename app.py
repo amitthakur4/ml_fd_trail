e96c543e00668b3289a63bcd37a17fd4a464a63b
amitthakur4 <amitthakur449@gmail.com>
2023-07-03 19:16:49 +0530

    added gitignore and readme and added port for windows error

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
-# from crypt import methods
+from flask import Flask, app
+from flask import Response
 
-app = Flask(__name__)
+app=Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/",methods=['GET','POST'])
 def index():
     return "Starting top to bottom deployment"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=9090)
